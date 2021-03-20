0743957fd1e6899b13eff4bc4f4b763e879c2d74
Joseph Daniel Rojas Flores <a2019113821@estudiantes.upsa.edu.bo>
2021-03-20 10:26:33 -0400

    Se creo el Modelo Product

---
+++ Parcial1_2019113821/Parcial1_RojasFloresJosephDaniel/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_RojasFloresJosephDaniel.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Ingrese entre 3 a 30")]
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString ="{0:C2}", ApplyFormatInEditMode =true)]
+        public Nullable<float> Price { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
+        public DateTime Date { get; set; }
+    }
+}
